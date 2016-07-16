09c82c8376ca19b084a8a5993442002d0b682b37
jadr2ddude <jadr2ddude@gmail.com>
2016-07-15 23:56:39 -0400

    Remove arduino-mk from arch setup script

--- archsetup.sh
+++ archsetup.sh
@@ -1,5 +1,5 @@
 su -s "pacman -S binutils make gcc fakeroot"
-yaourt -S ravi arduino arduino-mk
+yaourt -S ravi arduino
 echo "Now to add you to uucp. . ."
 su -c "gpasswd -a "$(whoami)" uucp"
 echo "Now to add you to lock. . ."
