44013ed27781d6b2bb06b241a65d159219252d29
Jaden Weiss <jadr2ddude@gmail.com>
2016-12-29 15:50:24 -0500

    Add camera stuff

---
+++ src/go/camera/shmeh.go
@@ -0,0 +1,11 @@
+package camera
+
+import (
+	"github.com/blackjack/webcam"
+	"log"
+	"image"
+)
+
+func getImage() image.Image {
+	return img
+}
--- src/go/can/can.go
+++ src/go/can/can.go
@@ -3,26 +3,20 @@ package can
 
 import (
 	"golang.org/x/sys/unix"
-	"logger"
 	"os"
-	"shutdown"
-	"failutil"
 	"github.com/howeyc/crc16"
 	"encoding/binary"
 )
 
 var fd int
-var log Logger
 func init() {
-	logger.NewLogger("CAN")
 	f,err := unix.Socket(AF_CAN, SOCK_RAW, CAN_RAW)		//Make a SocketCAN socket
-	failutil.CrashIfErr(err)	//Crash if error
-	err = unix.Bind(f,&SockaddrCAN{Ifindex: 0})			//Bind it to all CAN interfaces
-	failutil.CrashIfErr(err)	//Crash if error
+	panic(err)					//Crash if error
+	err = unix.Bind(f,&SockaddrCAN{Ifindex: 0})		//Bind it to all CAN interfaces
+	panic(err)					//Crash if error
 }
 
 func crc(dat []byte) (out []byte) {
 	out = make(byte,2)
 	binary.LittleEdian.PutUint16(out,crc16.Checksum(dat,crc16.makeTable(0x4599)))
 }
-
