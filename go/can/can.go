56d50b24f3948f5ab48ea9ec5bab89cf88c5dd1f
jadr2ddude <jadr2ddude@gmail.com>
2017-02-13 16:49:31 -0500

    Meh

--- go/can/can.go
+++ go/can/can.go
@@ -1,18 +1,8 @@
-//System to work with the can bus using sys/unix
+//System to work with the can bus
 package can
 
 import (
-	"encoding/binary"
-	"github.com/howeyc/crc16"
-	"golang.org/x/sys/unix"
-	"os"
 )
-
-var fd int
-
 func init() {
-	f, err := unix.Socket(AF_CAN, SOCK_RAW, CAN_RAW) //Make a SocketCAN socket
-	panic(err)                                       //Crash if error
-	err = unix.Bind(f, &SockaddrCAN{Ifindex: 0})     //Bind it to all CAN interfaces
-	panic(err)                                       //Crash if error
+
 }
