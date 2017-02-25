8bbcafef099fddfd3f007791ac2041d308f73497
Alice Lai <allai@ctemc.org>
2017-02-25 16:07:14 -0500

    setting up CAN bus

--- Arduino/BottomSide/BottomSide.ino
+++ Arduino/BottomSide/BottomSide.ino
@@ -1,56 +1,48 @@
 #include <Servo.h>
-//#include <Tlc5940.h>
+#include <Metro.h>
+#include <FlexCAN.h>
 
-Servo serv0;
-const int laser = 4;
-const int claw = 5;
+Servo grab;
+Metro sysTimer = Metro(1);// milliseconds
+
+int laser = 4, valve = 5;
+FlexCAN CANbus(125000); //change baud rate later
+static CAN_message_t msg,rxmsg;
+unsigned int txTimer,rxTimer;
+
+
 
 void setup() {
-  //DEBUG SETUP
-  Serial.begin(115200);
-  Serial.println("Init");
+  //CAN BUS SETUP
+  CANbus.begin();
+  sysTimer.reset();
   
-  //TETHER SETUP
-  Serial3.begin(19200);
-  
-  pinMode(16,OUTPUT);
-  digitalWrite(16,LOW);
-
+  //LASER SETUP
   pinMode(laser, OUTPUT);
   digitalWrite(laser, LOW);
-  
-  /*TLC SETUP
-    Tlc.init();
-    Tlc.update(); */
     
   //SERVO SETUP
-  serv0.attach(23);
+  grab.attach(23); //revise pin number l8r
+}
+
+static void getMessage() {
   
-  Serial.println("Initialization Complete");
 }
 
 void turnValve() {
-  Serial3.println("Turning valve");
-  while (Serial3.available() < 2);
-  byte valve = Serial3.read();
+  byte value = rxmsg; 
 
-  switch(valve) {
-    case 7: analogWrite(claw, 128);
+  switch(value) {
+    case 7: analogWrite(valve, 128);
             delay(3000);
   }
 }
 
-
 //PROCESS TLC INPUT
 void processMotor(){
-  Serial.println("TLC Set");
-  while (Serial3.available() < 2);
+ 
   byte motor = 2*Serial3.read();
   char value = map(Serial3.read(),0,255,-128,127);
-  Serial.print("Motor ");
-  Serial.print(motor/2);
-  Serial.print(" = ");
-  Serial.println(value);
   int a, b;
   if (value < 0) {
     a = map(-long(value), 0, 128, 0, 4095);
@@ -59,42 +51,31 @@ void processMotor(){
     a = 0;
     b = map(long(value), 0, 127, 0, 4095);
   }
-//  Tlc.set(motor,a);
-//  Tlc.set(motor+1,b);
+;
 }
 
-void shineLaser() {
-  
-  while(Serial3.available()<2);
-  byte shine = Serial3.read();
+void shineLaser() {  
+  byte shine = ;
 
   switch(shine) {
     case 6: digitalWrite(laser, HIGH);
   }  
 }
 void servoSet(){
-  Serial.print("Set servo ");
-  while(Serial3.available()<2);
   byte servIndex = Serial3.read();
-  Serial.print(servIndex);
   byte servValue = map(Serial3.read(), 0, 255, 0, 180);
-  Serial.print(" to ");
-  Serial.println(servValue);
   switch (servIndex){
-    case 0: serv0.write(servValue); break;
+    case 0: grab.write(servValue); break;
   }
 }
 
 void tetherProcess(){
-  Serial.println("Processing Command");
-  //Serial.println(Serial3.read());
   switch(Serial3.read()){		//PROCESS COMMAND
     case 0: processMotor();break;				//TLC INPUT
-    case 1: Serial.println("TLCUpdate");//Tlc.update();break;	//TLC UPDATE
     case 4: servoSet();break;					//SET SERVO INPUTS
   }
 }
 
-void loop(){
+void loop(){ 
   if(Serial3.available()>0){tetherProcess();}
 }
