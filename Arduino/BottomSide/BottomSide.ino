8b4d909cd84953d4497802fb3e1adadc51689c28
Alice Lai <allai@ctemc.org>
2017-03-04 12:44:44 -0500

    creating msg struct not working

--- Arduino/BottomSide/BottomSide.ino
+++ Arduino/BottomSide/BottomSide.ino
@@ -6,11 +6,11 @@ Servo grab;
 Metro sysTimer = Metro(1);// milliseconds
 
 int laser = 4, valve = 5;
+int clawStuff [2] = {12, 13}; //valve + servo for grabbing
+
 FlexCAN CANbus(125000); //change baud rate later
 static CAN_message_t msg,rxmsg;
 unsigned int txTimer,rxTimer;
-
-
 
 void setup() {
   //CAN BUS SETUP
@@ -22,25 +22,28 @@ void setup() {
   digitalWrite(laser, LOW);
     
   //SERVO SETUP
-  grab.attach(23); //revise pin number l8r
+  grab.attach(23); //revise pin number later
 }
 
-static void getMessage() {
-  
+typedef struct{
+  byte msg [8];
+} message;
+
+void getMessage(void) {  
+  message rx = {CANbus.read(rxmsg)};
+}
+byte sendMessage(void) {
+  message txmsg;
+  return 0;
 }
 
-void turnValve() {
-  byte value = rxmsg; 
 
-  switch(value) {
-    case 7: analogWrite(valve, 128);
-            delay(3000);
-  }
+void claw(int motor, int value) {
+    analogWrite(clawStuff[motor], value);    
 }
 
 //PROCESS TLC INPUT
-void processMotor(){
- 
+void processMotor(){ 
   byte motor = 2*Serial3.read();
   char value = map(Serial3.read(),0,255,-128,127);
   int a, b;
@@ -51,22 +54,20 @@ void processMotor(){
     a = 0;
     b = map(long(value), 0, 127, 0, 4095);
   }
-;
+
+//  Tlc.set(motor,a);
+//  Tlc.set(motor+1,b);
 }
 
 void shineLaser() {  
-  byte shine = ;
-
-  switch(shine) {
-    case 6: digitalWrite(laser, HIGH);
-  }  
+  digitalWrite(laser, HIGH);
 }
 void servoSet(){
-  byte servIndex = Serial3.read();
-  byte servValue = map(Serial3.read(), 0, 255, 0, 180);
-  switch (servIndex){
-    case 0: grab.write(servValue); break;
-  }
+//  byte servIndex = getMessage();
+//  byte servValue = map(servIndex, 0, 255, 0, 180);
+//  switch (servIndex){
+//    case 0: grab.write(servValue); break;
+//  }
 }
 
 void tetherProcess(){
