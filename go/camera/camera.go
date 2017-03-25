55cde188fd32402b05d11108d83ca171a35d83bd
orangebaron <orangebaronroblox@gmail.com>
2017-03-25 13:39:59 -0400

    add camera

---
+++ go/camera/camera.go
@@ -0,0 +1,29 @@
+package controller
+
+import (
+	"../controller"
+	"../startup"
+	"../util"
+	"github.com/blackjack/webcam"
+)
+
+var camLoc string
+
+func init() {
+	startup.NewTask(1, func() error { //Set up can flag parsing
+		flag.StringVar(&camLoc, "cam", "video0", "Cam connection")
+		return nil
+	})
+	startup.NewTask(252, func() error { //Set up can flag parsing
+		cam, err := webcam.Open(camLoc)
+		util.UhOh(err)
+		go func() {
+			for {
+				sleep(500) //send the img twice a second
+				data, err := cam.ReadFrame()
+				util.UhOh(err)
+				controller.SendData(data)
+			}
+		}()
+	})
+}
