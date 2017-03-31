27dfc383c88a188ac4b15c4f0bda06cd7ba9e75d
Jaden Weiss <jadr2ddude@gmail.com>
2017-03-30 22:24:02 -0400

    fix motor driver bugs

--- MotorDriver/driver.c
+++ MotorDriver/driver.c
@@ -2,88 +2,89 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 #include <util/twi.h>
+#include <stdbool.h>
 
-uint8 pinstates[16];
-uint8 ctr=0;
+uint8_t pinstates[16];
+uint8_t ctr=0;
 
 //Macro to write to a pin
 #define pinWrite(port,pin,val) PORT##port =  val * (val << pin)
 
 //Define pins
-#define PIN0 D, 0
-#define PIN1 D, 1
-#define PIN2 D, 2
-#define PIN3 D, 3
-#define PIN4 D, 4
-#define PIN5 B, 7
-#define PIN6 D, 5
-#define PIN7 D, 6
-#define PIN8 D, 7
-#define PIN9 B, 0
-#define PIN10 C, 4
-#define PIN11 C, 2
-#define PIN12 C, 1
-#define PIN13 C, 0
-#define PIN14 B, 5
-#define PIN15 B, 4
-#define PIN16 B, 3
-#define PIN17 B, 1
-
-//Pin selection macro (e.g. PINx(0) -> PIN0)
-#define PINx(pin) PIN##pin
+#define MPIN0 D , 0
+#define MPIN1 D, 1
+#define MPIN2 D, 2
+#define MPIN3 D, 3
+#define MPIN4 D, 4
+#define MPIN5 B, 7
+#define MPIN6 D, 5
+#define MPIN7 D, 6
+#define MPIN8 D, 7
+#define MPIN9 B, 0
+#define MPIN10 C, 4
+#define MPIN11 C, 2
+#define MPIN12 C, 1
+#define MPIN13 C, 0
+#define MPIN14 B, 5
+#define MPIN15 B, 4
+#define MPIN16 B, 3
+#define MPIN17 B, 1
 
 //Process a pin
-#define PINproc(pin) pinWrite( PINx(pin) , pinstates[ pin ] < ctr )
+#define PINproc(port, pin, pinn) p ## port = ( ( p ## port ) && ( ~ ( 1 << pin) ) ) || ( ( ( pinstates[ pinn ] < ctr ) ) << pin )
 
 ISR (TIMER1_OVF_vect) { //On timer overflow, process pins
-	PINproc(0);
-	PINproc(1);
-	PINproc(2);
-	PINproc(3);
-	PINproc(4);
-	PINproc(5);
-	PINproc(6);
-	PINproc(7);
-	PINproc(8);
-	PINproc(9);
-	PINproc(10);
-	PINproc(11);
-	PINproc(12);
-	PINproc(13);
-	PINproc(14);
-	PINproc(15);
+	uint8_t pa = PORTA;
+	uint8_t pb = PORTB;
+	uint8_t pc = PORTC;
+	uint8_t pd = PORTD;
+	PINproc(d, 0, 0);
+	PINproc(d, 1, 1);
+	PINproc(d, 2, 2);
+	PINproc(d, 3, 3);
+	PINproc(d, 4, 4);
+	PINproc(b, 7, 5);
+	PINproc(d, 5, 6);
+	PINproc(d, 6, 7);
+	PINproc(d, 7, 8);
+	PINproc(b, 0, 9);
+	PINproc(c, 4, 10);
+	PINproc(c, 2, 11);
+	PINproc(c, 1, 12);
+	PINproc(c, 0, 13);
+	PINproc(b, 5, 14);
+	PINproc(b, 4, 15);
+	PINproc(b, 3, 16);
+	PINproc(b, 1, 17);
+	PORTA = pa;
+	PORTB = pb;
+	PORTC = pc;
+	PORTD = pd;
 	ctr++;
-	TWCR |= b10000000;
-}
-
-void setupTimer() {
-	TCCR1A = TIMER1_PWM_INIT;
-	TCCR1B |= TIMER1_CLOCKSOURCE;
 }
 
 void setupTWI() {
 	TWAR = 40;		//Set slave address
 	TWBR = 8;		//Set speed to 128kbps
-	TWCR |= b11000101;	//Set TWI control flags to enable TWI
+	TWCR |= 0b11000101;	//Set TWI control flags to enable TWI
 }
 
-const int ADDR = 0;
-const int VAL = 1;
-int editstat=ADDR;
+#define STAT_ADDR 0
+#define STAT_VAL 1
+int editstat=STAT_ADDR;
 int motid;
 
 ISR (TWI_vect) {
-	if(editstat==ADDR) {
+	if(editstat==STAT_ADDR) {
 		motid=TWDR;
-		editstat=VAL;
-	}else if(editstat==VAL) {
+		editstat=STAT_VAL;
+	}else if(editstat==STAT_VAL) {
 		pinstates[motid]=TWDR;
 	}
-	TWCR |= b10000000;
+	TWCR |= _BV(7);
 }
 
 void main() {
-	setupTimer();
 	setupTWI();
 	while(true){
 		sleep_mode();
