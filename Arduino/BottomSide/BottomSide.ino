f5291fae6337438725f2c78effed5db9de8a3aec
orangebaron <orangebaronroblox@gmail.com>
2017-04-01 16:18:16 -0400

    undo what I just did
    whoops wrong file

--- Arduino/BottomSide/BottomSide.ino
+++ Arduino/BottomSide/BottomSide.ino
@@ -4,23 +4,6 @@
 #include <FlexCAN.h>
 #include <Wire.h>
 
- #define PIN0 D, 0
- #define PIN1 D, 2
- #define PIN2 D, 3
- #define PIN3 D, 4
- #define PIN4 B, 7
- #define PIN5 D, 5
- #define PIN6 D, 6
- #define PIN7 B, 0
- #define PIN8 C, 4
- #define PIN9 C, 2
- #define PIN10 C, 1
- #define PIN11 C, 0
- #define PIN12 B, 5
- #define PIN13 B, 4
- #define PIN14 B, 3
- #define PIN15 B, 1
-
 Servo grab;
 Metro sysTimer = Metro(1);
 
