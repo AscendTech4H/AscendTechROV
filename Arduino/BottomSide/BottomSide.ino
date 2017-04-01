c9eb593437cc7b745d533b36121503bfc943fa7f
Alice Lai <allai@ctemc.org>
2017-03-31 21:52:35 -0400

    change baud rate and pin numbers later

--- Arduino/BottomSide/BottomSide.ino
+++ Arduino/BottomSide/BottomSide.ino
@@ -1,12 +1,12 @@
+
 #include <Servo.h>
 #include <Metro.h>
 #include <FlexCAN.h>
-
+#include <Wire.h>
 Servo grab;
 Metro sysTimer = Metro(1);
 
 int laser = 4, valve = 5;
-int clawStuff [2] = {12, 13}; //valve + servo for grabbing
 
 FlexCAN CANbus(125000); //change baud rate later
 static CAN_message_t msg,rxmsg;
@@ -28,16 +28,56 @@ void setup() {
   grab.attach(23); //revise pin number later
 }
 
-void getMessage(void) {  
+void getMessage() {  
   if (!rxTimer) {
     while (CANbus.read(rxmsg)) {
       for (int i = 0; i < 8; i++) {
         input[i] = rxmsg.buf[i];
       } 
     }
-    
   }
 }
+
+void claw(int motor, int value) { //closed = 180, open = 90
+    analogWrite(motor, value);    
+}
+
+//PROCESS ATTINY INPUT
+void processMotor(int motor, int value){ 
+  int v = map(value,0,255,-255,255);
+  int l,r;
+  if(v>0) {
+    l = v;
+    r = 0;
+  } else {
+    l = 0;
+    r = v;
+  }
+  
+  Wire.beginTransmission(0);
+  Wire.write((motor*2));
+  Wire.write(l);
+  Wire.write((motor*2)+1);
+  Wire.write(r);
+  Wire.endTransmission();
+}
+
+void shineLaser() {  
+  digitalWrite(laser, HIGH);
+}
+
+void loop(){ 
+    getMessage();
+    switch (input[0]) {
+      case 0: processMotor(input[1], input[2]);
+      case 1: shineLaser(); break; 
+      case 2: claw(input[1], input[2]); break;
+
+    }    
+}  
+
+
+/*
 void sendMessage(byte input [8]) {
   if (!txTimer) {
     txTimer = 100;
@@ -54,48 +94,4 @@ void sendMessage(byte input [8]) {
       msg.buf[0]++;
     }
   }
-}
-
-
-void claw(int motor, int value) { //closed = 180, open = 90
-    analogWrite(clawStuff[motor], value);    
-}
-
-//PROCESS TLC INPUT
-void processMotor(){ 
-  byte motor = 2*Serial3.read();
-  char value = map(Serial3.read(),0,255,-128,127);
-  int a, b;
-  if (value < 0) {
-    a = map(-long(value), 0, 128, 0, 4095);
-    b = 0;
-  } else {
-    a = 0;
-    b = map(long(value), 0, 127, 0, 4095);
-  }
-
-//  Tlc.set(motor,a);
-//  Tlc.set(motor+1,b);
-}
-
-void shineLaser() {  
-  digitalWrite(laser, HIGH);
-}
-void servoSet(){
- byte servIndex = rxmsg.buf[0];
- byte servValue = map(servIndex, 0, 255, 0, 180);
-  switch (servIndex){
-    case 0: grab.write(servValue); break;
-  }
-}
-
-void tetherProcess(){
-  switch(Serial3.read()){		//PROCESS COMMAND
-    case 0: processMotor();break;				//TLC INPUT
-    case 4: servoSet();break;					//SET SERVO INPUTS
-  }
-}
-
-void loop(){ 
-  if(Serial3.available()>0){tetherProcess();}
-}
+}*/
