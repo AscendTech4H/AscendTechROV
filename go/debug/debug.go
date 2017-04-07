210c1703597805c15cf317bd8ddcf54e9d56cd6c
Jaden Weiss <jadr2ddude@gmail.com>
2017-04-06 20:58:49 -0400

    add to debugging

--- go/debug/debug.go
+++ go/debug/debug.go
@@ -2,6 +2,10 @@ package debug
 
 import (
 	"flag"
+	"html"
+	"log"
+	"net/http"
+	"sync"
 
 	"../startup"
 )
@@ -9,9 +13,61 @@ import (
 //Verbose indicates whether verbose debugging is enabled
 var Verbose bool
 
+//Log buffer
+type logBuf struct {
+	buf  []byte
+	outs []http.ResponseWriter
+	lck  sync.Mutex
+}
+
+func (l *logBuf) Write(b []byte) (int, error) {
+	l.lck.Lock()
+	defer l.lck.Unlock()
+	s := html.EscapeString(string(b))
+	for i, w := range l.outs {
+		_, err := w.Write([]byte(s))
+		w.(http.Flusher).Flush()
+		if err != nil {
+			l.outs[i] = nil
+		}
+	}
+	re := []http.ResponseWriter{}
+	for _, w := range l.outs {
+		if w != nil {
+			re = append(re, w)
+		}
+	}
+	l.outs = re
+	l.buf = append(l.buf, b...)
+	return len(b), nil
+}
+func (l *logBuf) Add(w http.ResponseWriter) {
+	l.lck.Lock()
+	defer l.lck.Unlock()
+	l.outs = append(l.outs, w)
+}
+
+var lbuf *logBuf
+
+//VLog - log if verbose
+func VLog(s string) {
+	if Verbose {
+		log.Println(s)
+	}
+}
+
 func init() {
 	startup.NewTask(1, func() error { //Set up can flag parsing
 		flag.BoolVar(&Verbose, "verbose", false, "enable verbose debugging")
+		return nil
+	})
+	startup.NewTask(1, func() error {
+		lbuf = new(logBuf)
+		lbuf.buf = []byte{}
+		lbuf.outs = []http.ResponseWriter{}
+		http.HandleFunc("/debug", func(w http.ResponseWriter, r *http.Request) {
+			lbuf.Add(w)
+		})
 		return nil
 	})
 }
