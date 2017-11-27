e4c794238e162f777b1fe387068ba41138fdf268
orangebaron <saalws@ctemc.org>
2017-11-26 20:17:47 -0500

    addresses

--- go/conf/arduinoaccel.go
+++ go/conf/arduinoaccel.go
@@ -9,13 +9,23 @@ import (
 //ArduinoAccel is an accelerometer configuration
 type ArduinoAccel struct {
 	Name string
-	Addr []byte
+	Addr uint8
 }
 
 var arduinoAccelDirectiveProcessor = bracketconf.NewDirectiveProcessor(
 	nameDirective,
 	bracketconf.Directive{Name: "addr", Callback: func(object interface{}, ans ...bracketconf.ASTNode) {
-		//TODO: address
+		if len(ans) == 1 {
+			i := ans[0].Int()
+			if i < 0 || i > 127 {
+				panic(bracketconf.ConfErr{Pos: ans[0].Position(), Err: errors.New("Address must be between 0 and 127 inclusive")})
+			}
+			object.(*ArduinoAccel).Addr = uint8(i)
+		} else if len(ans) == 0 {
+			panic(errors.New("Address directive has no arguments"))
+		} else {
+			panic(bracketconf.ConfErr{Pos: ans[0].Position(), Err: errors.New("Address directive has too many arguments")})
+		}
 	}},
 )
 
